b392ec9cead7a16814e92a6d4ce8ead2fd43e3ac
KeithLamb <keithianlamb@gmail.com>
2024-06-12 15:19:52 -0500

    Built Project models for api/project,resource, and task next  routing of the api/project,resource, and task.

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,18 @@
 // build your `Project` model here
+const db = require('../../data/dbConfig')
+
+function getAll() {
+    return db('projects')
+}
+
+function create(project) {
+    return db('projects').insert(project)
+        .then(([id]) => {
+            return db('projects').where({ project_id: id }).first()
+        })
+}
+
+module.exports ={
+    getAll,
+    create
+}
--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,17 @@
 // build your `Resource` model here
+const db = require('../../data/dbConfig')
+
+function getAll() {
+    return db('resources')
+}
+
+function create(resource) {
+    return db('resources').insert(resource)
+        .then(([id]) => {
+            return db('resources').where({ resource_id: id }).first()
+        })
+}
+module.exports = {
+    getAll,
+    create
+}
